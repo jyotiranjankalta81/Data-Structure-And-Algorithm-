310c2357ea27d0f350ca37f55aac26d75191c7fc
Jyoti Ranjan Kalta <jyotiranjankalta81@gmail.com>
2021-11-18 17:19:33 +0530

    reverse a stack

--- learningMaterial/stack/reverseasentence.cpp
+++ learningMaterial/stack/reverseasentence.cpp
@@ -1,40 +1,47 @@
-// Reverse a sentence using stack 
+// Reverse a sentence using stack
 // let the sentence is:----> Hey,how are you doing?
 // then the sentence is:-->doing? you are how Hey,
 
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
 
-void reverseSentence(string s){
+void reverseSentence(string s)
+{
+    // define a string stack
     stack<string> st;
+    // traverse the entire string
     for (int i = 0; i < s.length(); i++)
     {
-        string word="";
-        while (s[i]!=' ' && i<s.length())
+        // here we store every word after breake e.g Hey
+        string word = "";
+        // traverse the string till not geeting space and i must be less than length
+        while (s[i] != ' ' && i < s.length())
         {
-            word+=s[i];
+            // now we get a word
+            word += s[i];
+            // and increment i
             i++;
         }
+        // single word push into the stack
         st.push(word);
-        
     }
-    while(!st.empty()){
-
-        cout<<st.top()<<" ";
+    // now run the stack till stack geeting empty
+    while (!st.empty())
+    {
+        // now print the stack
+        cout << st.top() << " ";
+        // now remove the top element from the stack
         st.pop();
-
-    }cout<<endl;
-    
+    }
+    cout << endl;
 }
-int main(){
+int main()
+{
 
     // stack<int> st;
-    string s = " Hey , how are you doing ?";
+    string s = " Hey , how are you doing?";
     reverseSentence(s);
-
-
-
 
     return 0;
 }
