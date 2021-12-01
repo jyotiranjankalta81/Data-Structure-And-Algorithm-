b56c7257b22eba49a567a589df5c4c67bb6c60e2
Jyoti Ranjan Kalta <jyotiranjankalta81@gmail.com>
2021-12-01 10:59:19 +0530

    cherry

---
+++ Codeforces/Codeforces Round/A/Cherry.cpp
@@ -0,0 +1,130 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define all(x) (x).begin(), (x).end()
+#define uniq(v) (v).erase(unique(all(v)), (v).end())
+#define sz(x) (int)((x).size())
+#define fr first
+#define sc second
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define mem1(a) memset(a, -1, sizeof(a))
+#define mem0(a) memset(a, 0, sizeof(a))
+#define ppc __builtin_popcount
+#define ppcll __builtin_popcountll
+
+template <typename T1, typename T2>
+istream &operator>>(istream &in, pair<T1, T2> &a)
+{
+    in >> a.fr >> a.sc;
+    return in;
+}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &out, pair<T1, T2> a)
+{
+    out << a.fr << " " << a.sc;
+    return out;
+}
+template <typename T, typename T1>
+T amax(T &a, T1 b)
+{
+    if (b > a)
+        a = b;
+    return a;
+}
+template <typename T, typename T1>
+T amin(T &a, T1 b)
+{
+    if (b < a)
+        a = b;
+    return a;
+}
+
+const long long INF = 1e18;
+const int32_t M = 1e9 + 7;
+const int32_t MM = 998244353;
+
+const int N = 0;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int ans = 0;
+    rep(i, 0, n)
+    {
+        cin >> a[i];
+        if (i)
+        {
+            amax(ans, a[i] * a[i - 1]);
+        }
+    }
+    cout << ans << "\n";
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+#ifdef SIEVE
+    sieve();
+#endif
+#ifdef NCR
+    init();
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
+
+#include <bits/stdc++.h>
+typedef long long int ll;
+const unsigned int MOD = 1000000007;
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    for (int tt = 0; tt < t; tt++)
+    {
+        int n;
+        cin >> n;
+
+        ll a[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        ll ans = INT_MIN;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            ans = max(a[i] * a[i + 1], ans);
+        }
+        cout << ans << "\n";
+    }
+#ifndef ONLINE_JUDGE
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+    return 0;
+}
