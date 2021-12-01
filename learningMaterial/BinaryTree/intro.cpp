d5f29a9eb3ab5c9d847a7e49fec231585b0ca1d2
Jyoti Ranjan Kalta <jyotiranjankalta81@gmail.com>
2021-12-01 10:01:36 +0530

    pizzaforces

--- learningMaterial/BinaryTree/intro.cpp
+++ learningMaterial/BinaryTree/intro.cpp
@@ -1,22 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Node{
+struct Node
+{
+    // in this node we store some integer data
     int data;
-    struct Node* left;
-    struct Node* right;
+    // make one pointer to point left child and another point to right child
+    struct Node *left;
+    struct Node *right;
 
-    Node(int val){
-        data=val;
-        left=NULL;
+    Node(int val)
+    {
+        // this node value assign to data
+        data = val;
+        // left child initialize with null and right also same
+        left = NULL;
         right = NULL;
     }
 };
 
-int main(){
-    struct Node* root = new Node(1);
-    root->left=new Node(2);
-    root->right= new Node(3);
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
 }
--- learningMaterial/BinaryTree/levelOrder.cpp
+++ learningMaterial/BinaryTree/levelOrder.cpp
@@ -1,15 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-#include<queue>
+#include <queue>
 
-struct Node{
+struct Node
+{
     int data;
-    struct Node* left;
-    struct Node* right;
+    struct Node *left;
+    struct Node *right;
 
-    Node(int val){
-        data=val;
-        left=NULL;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
         right = NULL;
     }
 };
@@ -43,90 +45,80 @@ struct Node{
 //             {
 //                 q.push(node->right);
 //             }
-            
-            
 //         }else if(!q.empty()){
 //             q.push(NULL);
 //         }
 //     }
 // }
-
 // Find the sum of nodes at Kth level
-        //       5
-        //     /   \
+//       5
+//     /   \
         //    6     3
-        //  /   \  /  \
+//  /   \  /  \
         // 9     1 2   1
-        //      /  \                   let k=3;
-        //    2      6                 Sum of k nodes = 2+6 = 8
-        //  /  \      \
+//      /  \                   let k=3;
+//    2      6                 Sum of k nodes = 2+6 = 8
+//  /  \      \
         // 3     5     7
-
-
 // create a function
-int sumAtk(Node* root, int k){
+int sumAtk(Node *root, int k)
+{
     // check root null or not
-    if (root==NULL)
+    if (root == NULL)
     {
         return -1;
     }
-    queue<Node*>q;
+    queue<Node *> q;
     q.push(root);
     q.push(NULL);
     int level = 0;
-    int sum =0;
+    int sum = 0;
 
     // make a loop & check queue is empty or not
     while (!q.empty())
     {
-        Node* node = q.front();
+        Node *node = q.front();
         q.pop();
 
-        if (node!=NULL)
-        {
+        if (node != NULL)
+
+                {
             // check level ==k
-            if (level==k)
+            if (level == k)
             {
-                sum+= node->data;
+                sum += node->data;
             }
-            if(node->left){
+            if (node->left)
+            {
                 q.push(node->left);
             }
             if (node->right)
             {
                 q.push(node->right);
             }
-            
-            
-        }else if (!q.empty())
+        }
+        else if (!q.empty())
         {
             q.push(NULL);
             level++;
         }
-        
-        
     }
     return sum;
-    
-    
-
 }
 
-int main(){
-    struct Node* root = new Node(1);
-    root->left=new Node(2);
-    root->right= new Node(3);
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
     root->right->left = new Node(6);
     root->right->right = new Node(7);
 
-
-
     // Level Order Traversal
     // printLevelOrder(root);
 
-
     // Sum at Kth Level
-    cout<<sumAtk(root,2);
+    cout << sumAtk(root, 2);
 }
